25be37fffc2b9aa11fad2eb935adb63a5e2662b2
camillac <32921714+camillac@users.noreply.github.com>
2017-12-05 21:49:06 -0500

    Update Cyclops.java

--- v2/Cyclops.java
+++ v2/Cyclops.java
@@ -1,9 +1,9 @@
-public class Goblin extends Monster {
+public class Cyclops extends Monster {
     //constructor
-    public Goblin() {
-	health = 250;
+    public Cyclops() {
+	health = 300;
 	strength = (int) (Math.random() * 45 + 20);
 	defense = 20;
-	attackRate = 1;
+	attackRate = 0.8;
     }
 }
