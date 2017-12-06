f71354845936a4db6c00f219b33f87a58d7c7fe0
camillac <32921714+camillac@users.noreply.github.com>
2017-12-05 21:29:16 -0500

    Create Warrior.java

---
+++ v2/Warrior.java
@@ -0,0 +1,9 @@
+public class Warrior extends Protagonist {
+    public Warrior (String input) {
+      name = input;
+      health = 200;
+      strength = 100;
+      defense = initdefense = 40;
+      attack = initattack = 0.4;
+    }
+}
