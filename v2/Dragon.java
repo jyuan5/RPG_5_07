0f681695e9645759c4c9442092a9e7832b4ae90d
camillac <32921714+camillac@users.noreply.github.com>
2017-12-05 21:35:25 -0500

    Update Dragon.java

--- v2/Dragon.java
+++ v2/Dragon.java
@@ -1,9 +1,9 @@
 public class Dragon extends Monster {
     //constructor
     public Monster() {
-		  health = 150;
-		  strength = (int) (Math.random() * 45 + 20);
-		  defense = 75;
-		  attackRate = 1;
+	health = 150;
+	strength = (int) (Math.random() * 45 + 20);
+	defense = 75;
+	attackRate = 1;
     }
 }
