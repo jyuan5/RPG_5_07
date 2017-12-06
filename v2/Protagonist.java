0aaaf0632670e0ff0575ae9ac0c26643dead9faf
Vincent <vchi90@stuy.edu>
2017-12-05 21:20:33 -0500

    Protagonis.java

--- v2/Protagonist.java
+++ v2/Protagonist.java
@@ -1,19 +1,27 @@
 public class Protagonist extends Character { //Not Tested
-    public String name;
+    private String name;
     
     public int initdefense;
     public double initattack;
+
+    public Protagonist (String input) {
+    name = input;
+    health = 125;
+    strength = 100;
+    defense = initdefense = 40;
+    attackRate = initattack = 0.4;
+    }
     
     public String getName(){ return name; }
     
     public int special = 5;
     public void specialize(){
         defense -= special;
-        attack += special;
+        attackRate += special;
     }
     
     public void normalize(){
         defense = initdefense;
-        attack = initattack;
+        attackRate = initattack;
     }
 }
