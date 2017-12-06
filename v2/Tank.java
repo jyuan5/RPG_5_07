4b4488e69efe4e9471d62a85cdb38b2f154a2bfb
camillac <32921714+camillac@users.noreply.github.com>
2017-12-05 21:27:00 -0500

    Update Tank.java

--- v2/Tank.java
+++ v2/Tank.java
@@ -4,6 +4,6 @@ public class Tank extends Protagonist {
       health = 125;
       strength = 100;
       defense = initdefense = 80;
-      attack = initattack = 0.8;
+      attack = initattack = 0.4;
     }
 }
