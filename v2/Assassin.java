9b3d6310a8ea8494044d0fa35814b2ab188202c9
Vincent <vchi90@stuy.edu>
2017-12-05 20:49:27 -0500

    Assassin.java

---
+++ v2/Assassin.java
@@ -0,0 +1,9 @@
+public class Assassin extends Protagonist {
+    public Assassin (String input) {
+    name = input;
+    health = 125;
+    strength = 100;
+    defense = initdefense = 40;
+    attack = initattack = 0.8;
+    }
+}
