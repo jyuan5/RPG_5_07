d8c7f39643205f275f820a59b05d07fab3afd66d
camillac <32921714+camillac@users.noreply.github.com>
2017-12-05 21:26:38 -0500

    Create Tank.java

---
+++ v2/Tank.java
@@ -0,0 +1,9 @@
+public class Tank extends Protagonist {
+    public Tank (String input) {
+      name = input;
+      health = 125;
+      strength = 100;
+      defense = initdefense = 80;
+      attack = initattack = 0.8;
+    }
+}
