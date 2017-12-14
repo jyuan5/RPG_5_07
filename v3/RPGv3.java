e0a92695b4e88ee605dfd656f7d2b2ca8f361b74
Vincent <34114571+vchi90@users.noreply.github.com>
2017-12-13 23:21:21 -0500

    Create RPGv3.java

---
+++ v3/RPGv3.java
@@ -0,0 +1,2 @@
+public class RPGv3 { //NOT TESTED Camilla or Jessie finish up this homework please! Continue from 3. in his homework page.
+    Protagonist p = new Protagonist(); // Finish this and get the error message.
